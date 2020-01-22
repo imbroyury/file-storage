c568d6d62e8e020cbb223b49c204a2e54962f102
Yury Imbro <imbroyury@gmail.com>
2020-01-22 21:05:27 +0300

    credentials refactored + new URL

--- server/EmailService.js
+++ server/EmailService.js
@@ -3,22 +3,24 @@ import { HTTP_URL } from '../src/shared/hosts';
 import emailCredentials from './email-credentials.json';
 
 var transport = nodemailer.createTransport({
-    service: 'Yandex',
+    service: emailCredentials.service,
     auth: {
-        ...emailCredentials,
+        user: emailCredentials.user,
+        pass: emailCredentials.pass,
     }
 });
 
 const sendConfirmationEmail = (email, login, token) => new Promise((resolve, reject) => {
-    const link = `${HTTP_URL}/confirmEmail?confirmationToken=${token}`;
+    const url = new URL('confirmEmail', HTTP_URL);
+    url.searchParams.append('confirmationToken', token);
 
     const mailOptions = {
         to: email,
-        from: 'noreply-filestorage@yandex.by',
+        from: emailCredentials.from,
         subject : 'Welcome to FileStorage! Please confirm your email account to access the service',
         html: `
             Hello, ${login}!<br>
-            Please click <a href="${link}">here</a> to confirm your email.<br>
+            Please click <a href="${url.toString()}">here</a> to confirm your email.<br>
         `,
     }
 
